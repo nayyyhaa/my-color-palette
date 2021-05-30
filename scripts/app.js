210abd4a403c4373a08bece2d222d4cd346f5c9e
nayyyhaa <nehaguptang.ng@gmail.com>
2021-05-30 16:45:55 +0530

    feat: colorize sliders

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ let colorDivs = document.querySelectorAll(".color");
 let generateBtn = document.querySelector(".generate")
 let adjustBtns = document.querySelectorAll(".adjust");
 let sliders = document.querySelectorAll(".sliders");
+let closeBtns = document.querySelectorAll(".close-adjustments");
 /*functions*/
 
 /*generating color*/
@@ -38,14 +39,42 @@ function randomColor() {
 
         /*check luminance for hexText*/
         checkLuminence(generatedHex, hexText);
+
+        /*colorize sliders*/
+        let color = chroma(generatedHex);
+        let sliderItems = div.querySelectorAll(".sliders input");
+        const hue = sliderItems[0];
+        const saturation = sliderItems[1];
+        const brightness = sliderItems[2];
+        colorizeSliders(color, hue, saturation, brightness);
     })
+}
+
+/*colorize Sliders*/
+function colorizeSliders(color, hue, saturation, brightness) {
+    /*scale saturation*/
+    const noSat = color.set('hsl.s',0);
+    const fullSat = color.set('hsl.s',1);
+    const scaleSat = chroma.scale([noSat,color,fullSat]);
+
+    /*scale lightness*/
+    const midLight = color.set('hsl.l',0.5);
+    const scaleLight = chroma.scale(["black",midLight,"white"]);
+
+    /*update input color*/
+    saturation.style.backgroundImage = `linear-gradient(to right, ${scaleSat(0)}, ${scaleSat(0.5)}, ${scaleSat(1)})`;
+    brightness.style.backgroundImage = `linear-gradient(to right, ${scaleLight(0)}, ${scaleLight(0.5)}, ${scaleLight(1)})`;
+    hue.style.backgroundImage = `linear-gradient(to right, rgb(204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
 
 /*open slider*/
 function toggleSlider(index) {
-    console.log(sliders[index])
     let slider = sliders[index];
     slider.classList.toggle("open");
+}
+function closeSlider(index) {
+    let slider = sliders[index];
+    slider.classList.remove("open");
 }
 
 /*Event Listeners*/
@@ -54,6 +83,12 @@ generateBtn.addEventListener("click",randomColor);
 
 adjustBtns.forEach((adjust,index) => {
     adjust.addEventListener("click",() => {
-        toggleSlider(index)
+        toggleSlider(index);
     });
+})
+
+closeBtns.forEach((close,index) => {
+    close.addEventListener("click",() => {
+        closeSlider(index);
+    })
 })
